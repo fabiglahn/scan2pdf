26205a7691db69f087f9b2f8e156a7f74a42d8bf
“[fabiglahn] <[fabian-glahn@hotmail.de]>
2021-11-10 15:17:32 +0100

    ♻️ Create recognizeText function

--- src/app/pages/Scan/Scan.tsx
+++ src/app/pages/Scan/Scan.tsx
@@ -1,14 +1,31 @@
 import React, { useState } from 'react';
 import ImageInput from '../../components/ImageInput/ImageInput';
 import styles from './Scan.module.css';
+import { recognizeText } from '../../utils/ocr';
 
 function Scan() {
   const [imageURL, setImageURL] = useState<string | null>(null);
+  const [recognizedText, setRecognizedText] = useState<string | null>(null);
 
   return (
     <div className={styles.container}>
-      <ImageInput onImageUpload={setImageURL} />
-      <button className={styles.scan} disabled={imageURL === null}>
+      {recognizedText ? (
+        <p>{recognizedText}</p>
+      ) : (
+        <ImageInput onImageUpload={setImageURL} />
+      )}
+      <button
+        className={styles.scan}
+        disabled={imageURL === null}
+        onClick={() => {
+          if (imageURL) {
+            recognizeText(imageURL, ({ progress, status }) => {
+              console.log(progress, status);
+            }).then(setRecognizedText);
+          }
+        }}
+      >
+        {' '}
         SCAN
       </button>
       <a href="#" className={styles.link}>
