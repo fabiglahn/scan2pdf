81b462af333feb7e4022fa108bd1c148d326a393
“[fabiglahn] <[fabian-glahn@hotmail.de]>
2021-11-12 16:30:38 +0100

    ♻️ Fetch Documents

--- src/app/components/DocPreview/DocPreview.stories.tsx
+++ src/app/components/DocPreview/DocPreview.stories.tsx
@@ -12,17 +12,3 @@ export const Kaugummis = (): JSX.Element => (
     text="Lorem ipsum dolor sit amet consectetur adipisicing elit. Veniam aut placeat vel repellendus. Voluptates quos explicabo, alias quibusdam animi aut autem possimus laborum, officiis blanditiis modi consequuntur nobis soluta ea."
   />
 );
-
-export const Gorillas = (): JSX.Element => (
-  <DocPreview
-    title="Gorillas"
-    text="Lorem ipsum dolor sit amet consectetur adipisicing elit. Veniam aut placeat vel repellendus. Voluptates quos explicabo, alias quibusdam animi aut autem possimus laborum, officiis blanditiis modi consequuntur nobis soluta ea."
-  />
-);
-
-export const Lampen = (): JSX.Element => (
-  <DocPreview
-    title="Lampen"
-    text="Lorem ipsum dolor sit amet consectetur adipisicing elit. Veniam aut placeat vel repellendus. Voluptates quos explicabo, alias quibusdam animi aut autem possimus laborum, officiis blanditiis modi consequuntur nobis soluta ea."
-  />
-);
--- src/app/components/DocPreview/DocPreview.tsx
+++ src/app/components/DocPreview/DocPreview.tsx
@@ -1,32 +1,19 @@
 import React from 'react';
 import styles from './DocPreview.module.css';
 
-type DocPreviewProps = {
+export type Document = {
+  id?: number;
   title: string;
   text: string;
 };
 
-export default function DocPreview({
-  title,
-  text,
-}: DocPreviewProps): JSX.Element {
+function DocPreview({ title, text }: Document): JSX.Element {
   return (
-    <div className={styles.container}>
-      <h1 className={styles.h1}>Your documents</h1>
-      <div className={styles.wrapper}>
-        <article className={styles.doc}>
-          <h2>{title}</h2>
-          <p>{text}</p>
-        </article>
-        <article className={styles.doc}>
-          <h2>{title}</h2>
-          <p>{text}</p>
-        </article>
-        <article className={styles.doc}>
-          <h2>{title}</h2>
-          <p>{text}</p>
-        </article>
-      </div>
+    <div>
+      <h2>{title}</h2>
+      <p className={styles.text}>{text}</p>
     </div>
   );
 }
+
+export default DocPreview;
